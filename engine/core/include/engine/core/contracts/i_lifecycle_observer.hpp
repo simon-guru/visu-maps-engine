9597e9b5a72671cd32ead9c5e8f6a2ca70197356
Simão A.M <simon.o2000dev@gmail.com>
2026-04-21 22:27:06 +0100

    core: add lifecycle observer adapter over observability sink

---
+++ engine/core/include/engine/core/contracts/i_lifecycle_observer.hpp
@@ -0,0 +1,37 @@
+/*
+# Copyright (c) 2026. VISU LLC. All rights reserved.
+# Author: Simão A.Mayunga
+# Date: 21/04/2026
+# This file is part of VISU LLC.
+*/
+
+#pragma once
+
+#include "engine/core/types/engine_lifecycle_events.hpp"
+
+namespace vme::engine::core::contracts {
+
+/**
+ * @brief Observador especializado para eventos de lifecycle.
+ *
+ * Objetivo:
+ * - permitir consumo reativo dos eventos de lifecycle sem duplicar o
+ *   pipeline canônico de observabilidade (`IObservabilitySink`).
+ *
+ * Uso recomendado:
+ * - módulos que precisam reagir a transições (`render`, `gfx`, `tiles`, `services`)
+ *   implementam este contrato;
+ * - um adapter de observabilidade traduz `EngineObservabilityEnvelope`
+ *   para chamadas deste observer.
+ */
+class ILifecycleObserver {
+public:
+    virtual ~ILifecycleObserver() = default;
+
+    /**
+     * @brief Recebe evento de lifecycle já filtrado e tipado.
+     */
+    virtual void on_lifecycle_event(const types::EngineLifecycleEvent& event) = 0;
+};
+
+}  // namespace vme::engine::core::contracts
